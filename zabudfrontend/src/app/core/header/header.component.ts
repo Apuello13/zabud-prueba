c8688334fb93430116d1047fb3b4f8e0aa0e7e3d
apuelloo <93224618+apuelloo@users.noreply.github.com>
2021-12-19 01:38:37 -0500

    se agrega ajuste al header

--- zabudfrontend/src/app/core/header/header.component.ts
+++ zabudfrontend/src/app/core/header/header.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from '../service/user.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  nombreUsuario: string = '';
+  constructor(
+    private _user: UserService,
+    private router : Router
+  ) {}
 
   ngOnInit(): void {
+    this.getNombreUsuario();
   }
 
+  getNombreUsuario() {
+    this.nombreUsuario = this._user.getUser().nombre;
+  }
+
+  logOut(){
+    this._user.deleteUser();
+    this.router.navigateByUrl('/login');
+  }
 }
